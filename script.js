1d8eec56b9053a53ffd00d7c07ed22235751d521
Afnan9565 <98505187+Afnan9565@users.noreply.github.com>
2022-03-23 11:00:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// Changing the days script is below
+function monday() {
+    var monday = document.getElementById("monday");
+    if (monday.style.display === "none") {
+      monday.style.display = "block";
+    } else {
+      monday.style.display = "none";
+    }
+  }
+function tuesday() {
+    var tuesday = document.getElementById("tuesday");
+    if (tuesday.style.display === "none") {
+      tuesday.style.display = "block";
+    } else {
+      tuesday.style.display = "none";
+    }
+  }
+  function wednesday() {
+    var wednesday = document.getElementById("wednesday");
+    if (wednesday.style.display === "none") {
+      wednesday.style.display = "block";
+    } else {
+      wednesday.style.display = "none";
+    }
+  }
+  function thursday() {
+    var thursday = document.getElementById("thursday");
+    if (thursday.style.display === "none") {
+      thursday.style.display = "block";
+    } else {
+      thursday.style.display = "none";
+    }
+  }
+  function friday() {
+    var friday = document.getElementById("friday");
+    if (friday.style.display === "none") {
+      friday.style.display = "block";
+    } else {
+      friday.style.display = "none";
+    }
+  }
+  function saturday() {
+    var saturday = document.getElementById("saturday");
+    if (saturday.style.display === "none") {
+      saturday.style.display = "block";
+    } else {
+      saturday.style.display = "none";
+    }
+  }
+
+// Downloading the diary as a pdf script is written below
+
+function download(){
+    window.print();
+}
